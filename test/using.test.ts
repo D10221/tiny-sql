100642d530938c8d792fc5172ea53df2eceb8c29
Mr D <d10221@protonmail.com>
2020-01-22 08:22:59 +0200

    fix test connection string

--- test/using.test.ts
+++ test/using.test.ts
@@ -1,7 +1,7 @@
 import execSql from "../src/exec";
 import using from "../src/using";
 import connect from "../src/connect-fty";
-process.env.DB = `server=localhost;database=tempdb;user=sa;password=${process.env.SQL_SERVER_SA_PASSWORD};`;
+process.env.DB = `server=${process.env.SQL_SERVER_DATA_SOURCE};database=${process.env.SQL_SERVER_DATABASE};user=${process.env.SQL_SERVER_USER};password=${process.env.SQL_SERVER_PASSWORD};`;
 describe("useConnection", () => {
   it("works", async () => {
     const value = await using(connect)(getValue(1));
