d9547305797135b37ca97fe87452fcd2f70e8be0
Mr D <d10221@protonmail.com>
2020-01-22 08:24:08 +0200

    do not export internals

--- src/params/index.ts
+++ src/params/index.ts
@@ -1,8 +1,2 @@
-export { default as excludeKeys } from "./exclude-keys";
-export { default as filterKeys } from "./filter-keys";
-export { default as getFields } from "./get-fields";
 export { default as getParams } from "./get-params";
-export { default as getType } from "./get-type";
-export { default as isTediousParamLike } from "./is-tedious-param-like";
-export { default as notNullField } from "./not-null-field";
 export * from "./types";
