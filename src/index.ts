80b485d1ff4c66cd18a7c9f744fe62298f868f33
Mr D <d10221@protonmail.com>
2020-02-06 13:58:58 +0200

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 export { default as config } from "./config";
 export { default as connect } from "./connect";
-export { default as execSql, Result } from "./execSql";
+export { default as execSql, ExecParams, query, Result } from "./execSql";
+export { beginTransaction, commit, rollback, withTransaction } from "./transaction";
 export { default as using } from "./using";
-export {
-  beginTransaction,
-  commit,
-  rollback,
-  withTransaction,
-} from "./transaction";
+
