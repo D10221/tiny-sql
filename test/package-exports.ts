88ba528bd86880593be00dd9a2cf15cbbfaea6f1
Mr D <d10221@protonmail.com>
2020-02-14 09:22:43 +0200

    pretty

--- test/package-exports.ts
+++ test/package-exports.ts
@@ -1,14 +1,33 @@
-import { beginTransaction, commit, config, connect, ExecParams, execSql, query, Result, rollback, using, withTransaction } from "../";
+import {
+  beginTransaction,
+  commit,
+  config,
+  connect,
+  ExecParams,
+  execSql,
+  query,
+  Result,
+  rollback,
+  using,
+  withTransaction,
+} from "../";
 
 describe("typescript packagee", () => {
-    it("exports", () => {
-        let x : ExecParams | Result | undefined = undefined;
-        expect(typeof x).toBe("undefined")
-        expect([config,
-            connect,
-            execSql,
-            query,
-            beginTransaction, commit, rollback, withTransaction,
-            using].reduce((prev, next) => prev && typeof next === "function", true))
-    })
-})
+  it("exports", () => {
+    let x: ExecParams | Result | undefined = undefined;
+    expect(typeof x).toBe("undefined");
+    expect(
+      [
+        config,
+        connect,
+        execSql,
+        query,
+        beginTransaction,
+        commit,
+        rollback,
+        withTransaction,
+        using,
+      ].reduce((prev, next) => prev && typeof next === "function", true),
+    );
+  });
+});
